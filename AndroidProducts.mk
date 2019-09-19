ad9369cd40af9fe4cc6ed0b3ac223783f898c6c2
Marko Man <darkobas@gmail.com>
2019-09-19 10:13:31 +0200

    op6t: update launch combos
    Change-Id: I89c028f186330fd6c6573d0600868324cbd425c8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
     $(LOCAL_DIR)/omni_oneplus6t.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_oneplus6t-user \
+    omni_oneplus6t-userdebug \
+    omni_oneplus6t-eng \
